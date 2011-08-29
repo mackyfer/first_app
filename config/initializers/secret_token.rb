e56e098bf526548cadf98547321fabd17d6495dd
Ricardo McPherson <mackyfer@gmail.com>
2011-08-28 21:31:07 -0500

    first save

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsite::Application.config.secret_token = '4f41a00fa55e09de9ef6879e4d4cbd8443424af742681779af7a0fe320a9ea0d40d99f427796fc736c9cf702d7d00e95da4029a18da5cbaa4a585e0f76d75c2f'
